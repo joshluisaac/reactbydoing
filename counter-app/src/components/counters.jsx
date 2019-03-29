46fc56e026a885c798236ddb3969ae457d355904
Josh Uzo <nwankwo.joshua@gmail.com>
2019-03-30 10:14:49 +1100

    Migrated and consolidated project

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,16 +2,12 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-
   render() {
     //Object/argument destructuring
-    const {onRest, counters, onDelete, onIncrement} = this.props;
+    const { onRest, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        <button
-          onClick={onRest}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onRest} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
         {counters.map(counter => (
---
+++ projectmanager/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello Joshua! </h1>;
+ReactDOM.render(element, document.getElementById("root"));
+
+console.log(element);
